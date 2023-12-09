d7a17bf652a6f0516cf9b9de3fb8a6c2444bb64d
skleenn <113645968+skleenn@users.noreply.github.com>
2023-12-09 00:27:04 -0800

    im donee for today (up to video 4)

---
+++ frontend/parser.ts
@@ -0,0 +1,101 @@
+// deno-lint-ignore-file no-explicit-any
+import { Stmt, Program, Expr, BinaryExpr, NumericLiteral, Identifier, NullLiteral } from "./ast.ts";
+import { tokenize, Token, TokenType } from "./lexer.ts";
+
+export default class Parser{
+    private tokens: Token[] = [];
+
+    private not_eof(): boolean {
+        return this.tokens[0].type != TokenType.EOF;
+    }
+
+    private at(){
+        return this.tokens[0] as Token;
+    }
+
+    private eat(){
+        const prev = this.tokens.shift() as Token;
+        return prev;
+    }
+
+    private expect(type: TokenType, err: any){
+        const prev = this.tokens.shift() as Token;
+        if (!prev || prev.type != type){
+            console.error("Parser error:\n", err, prev, " - Expecting: ", type);
+            Deno.exit(1);
+        }
+        return prev;
+    }
+
+    public produceAST(sourceCode: string): Program{
+        this.tokens = tokenize(sourceCode);
+        const program: Program = {
+            kind: "Program",
+            body: [],
+        };
+        // Parse until end of file
+        while (this.not_eof()){
+            program.body.push(this.parse_stmt());
+        }
+
+        return program;
+    }
+
+    private parse_stmt(): Stmt {
+        // skip to parse_expr
+        return this.parse_expr();
+    }
+
+    private parse_expr(): Expr {
+        return this.parse_additive_expr();
+    }
+
+    private parse_additive_expr(): Expr{
+        let left = this.parse_multiplicative_expr();
+        while (this.at().value == "+" || this.at().value == "-"){
+            const operator = this.eat().value;
+            const right = this.parse_multiplicative_expr();
+            left = {
+                kind: "BinaryExpr",
+                left, right, operator
+            } as BinaryExpr;
+        }
+        return left;       
+    }
+
+    private parse_multiplicative_expr(): Expr{
+        let left = this.parse_primary_expr();
+        while (this.at().value == "/" || this.at().value == "*"|| this.at().value == "%"){
+            const operator = this.eat().value;
+            const right = this.parse_primary_expr();
+            left = {
+                kind: "BinaryExpr",
+                left, right, operator
+            } as BinaryExpr;
+        }
+        return left;       
+    }
+
+    private parse_primary_expr(): Expr{
+        const tk = this.at().type;
+
+        switch (tk){
+            case TokenType.Identifier:
+                return { kind: "Identifier", symbol: this.eat().value } as Identifier;
+            case TokenType.Null:
+                this.eat();
+                return { kind: "NullLiteral", value: "null" } as NullLiteral;
+            case TokenType.Number:
+                return { kind: "NumericLiteral", value: parseFloat(this.eat().value) } as NumericLiteral;
+            case TokenType.OpenParen: {
+                this.eat();
+                const value = this.parse_expr();
+                this.eat();
+                return value;
+            }
+            default:
+                console.error("Unexpted token found during parsing", this.at());
+                Deno.exit(1);
+        }
+    }
+}
---
+++ main.ts
@@ -0,0 +1,22 @@
+import Parser from "./frontend/parser.ts";
+import { evaluate } from "./runtime/interpreter.ts";
+
+repl();
+
+function repl(){
+    const parser = new Parser();
+    console.log("\nRepl v0.1");
+    while (true){
+        const input = prompt("> ");
+        // check for no user input or exit
+        if (!input || input.includes("exit")){
+            Deno.exit(1);
+        }
+
+        const program = parser.produceAST(input);
+
+        const result = evaluate(program);
+        console.log(result);
+
+    }
+}
